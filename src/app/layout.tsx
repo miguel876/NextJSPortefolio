635c179dc1b9c36bb396c9fb241c3dca21420281
Miguel <miguel87666@gmail.com>
2025-05-11 23:33:39 +0100

    added more data and responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({
-  variable: "--font-roboto",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased container mx-auto`}>
+      <body
+        className={`${montserrat.variable} antialiased container mx-auto px-4 xl:px-0`}
+      >
         {children}
       </body>
     </html>
